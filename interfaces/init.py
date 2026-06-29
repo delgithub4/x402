dedc916d826058825422d64700815bd978a1c13f
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 22:55:52 +0100

    Create init.py

---
+++ interfaces/init.py
@@ -0,0 +1,4 @@
+from .config_interface import ConfigInterface
+from .endpoint_interface import EndpointInterface
+from .health_interface import HealthInterface
+from .repository_interface import RepositoryInterface
