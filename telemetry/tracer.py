9331b14176f604824b5aebfe617aeb204d236e17
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 23:22:30 +0100

    Create tracer.py

---
+++ telemetry/tracer.py
@@ -0,0 +1,12 @@
+import time
+
+
+class RequestTracer:
+
+    def start(self):
+
+        return time.perf_counter()
+
+    def finish(self, started):
+
+        return time.perf_counter() - started
